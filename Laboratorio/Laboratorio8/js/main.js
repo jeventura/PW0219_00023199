576ea3cb76791abad62bc70c736375443de4ef20
JulioVentura <je.ventura@gmail.com>
2019-10-19 15:02:45 -0600

    agregando validacion para remover entrada

--- Laboratorio/Laboratorio8/js/main.js
+++ Laboratorio/Laboratorio8/js/main.js
@@ -1,6 +1,6 @@
 let carnet_field = document.querySelector("#carnet_field");
 let schedule_dropdown = document.querySelector("#schedule_field");
-let lete_switch = document.querySelector("#late_switch");
+let late_switch = document.querySelector("#late_switch");
 let submit_btn = document.querySelector("#submit_btn");
 let tbody = document.querySelector("#table_body");
 let carnet_regex = new RegExp("[0-9]{8}$");
@@ -12,35 +12,51 @@ let printArray = () => {
 
   student_list.forEach(elem => {
     let new_row = document.createElement("tr");
-    let dateTime = new Date()
+  
 
     let celda = document.createElement("td");
+    let celdaValidar = document.createElement("td");
     let btnEliminar = document.createElement("button");
+    let txtArea = document.createElement("input");
+
+    
 
     new_row.classList.add("table-active");
     new_row.innerHTML = `<td>${elem.carnet}</td>
     <td>${elem.horario}</td>
-    <td>${dateTime.toLocaleString()}</td>
-    <td>${elem.late}</td>`;
+    <td>${elem.hora.toLocaleString()}</td>
+    <td>${elem.tarde}</td>`;
 
     /*
      *personalizando btn
      */
 
+    txtArea.className = "form-control";
+    //btnEliminar.innerText = "Eliminar";
+
     btnEliminar.className = "btn btn-danger";
     btnEliminar.innerText = "Eliminar";
     btnEliminar.value = elem.id;
 
-    btnEliminar.addEventListener("click", event => {
-      let id_actual = event.target.value;
-
-      student_list.forEach((elem, pos) => {
-        if (id_actual == elem.id) {
-          student_list.splice(pos, 1);
-          printArray();
-        }
+    
+      btnEliminar.addEventListener("click", event => {
+        let id_actual = event.target.value;
+  
+        if (txtArea.value === elem.carnet) {
+        student_list.forEach((elem, pos) => {
+          if (id_actual == elem.id) {
+            student_list.splice(pos, 1);
+            printArray();
+          }
+        });
+      }else{
+        alert('Para borrar, confirme el numero de carnet')
+      }
       });
-    });
+    
+    celdaValidar.appendChild(txtArea);
+    new_row.appendChild(celdaValidar);
+    tbody.appendChild(new_row);
 
     celda.appendChild(btnEliminar);
     new_row.appendChild(celda);
@@ -48,29 +64,35 @@ let printArray = () => {
   });
 };
 
+let parseLateSwitch = (value) => {
+  if (value) {
+    return "Tardisimo";
+  
+  }
+    return "test";
+   
+};
+
 let add_student = (carnet, schedule, late) => {
+  let dateTime= new Date();
     student_list.push({
         id: serial,
         carnet: carnet,
         horario: schedule,
-        tarde: late
+        tarde: late,
+        hora:dateTime
       });
       serial++;
 };
 
 
-let parseLateSwitch = (value) => {
-  if (value) {
-    return "Tardisimo";
-  }
-  return "A tiempo";
-};
+
 
 submit_btn.addEventListener("click", () => {
   let carnet = carnet_field.value;
   let schedule =schedule_dropdown.options[schedule_dropdown.selectedIndex].text;
-  let late = parseLateSwitch(lete_switch.cheked);
-
+  let late = parseLateSwitch(late_switch.cheked);
+  
   if (carnet_regex.test(carnet)) {
     add_student(carnet, schedule, late);
     printArray();
